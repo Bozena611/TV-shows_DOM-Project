6233217645245471949115ef4ce00aa8132b246a
Bozena611 <bozena.vuckovic@gmail.com>
2021-03-11 00:41:56 +0100

    added background + title fix

--- script.js
+++ script.js
@@ -1,15 +1,20 @@
-//You can edit ALL of the code here
 function setup() {
   const allEpisodes = getAllEpisodes();
   //console.log (allEpisodes);
   makePageForEpisodes(allEpisodes);
 }
 
+function formatSeasonNum(num){
+	if (num<10) {
+		return "0" + num;
+	} else {
+		return num;
+	}
+}
+
 function makePageForEpisodes(allEpisodes) {
 	const rootElem = document.getElementById("root");
   //console.log(allEpisodes[0].id)
-  //rootElem.textContent = `Got ${allEpisodes.length} episode(s)`;
-  
   let episodesContainer = document.createElement("div");
 	episodesContainer.setAttribute("id", "episodes-container");
 	rootElem.appendChild(episodesContainer);
@@ -17,19 +22,18 @@ function makePageForEpisodes(allEpisodes) {
 	allEpisodes.forEach((element) => {
 		let episodeDiv = document.createElement("div");
 		episodeDiv.setAttribute("class", "episode-div")
-		
-
 		let episodeTitle = document.createElement("h2");
 		episodeTitle.setAttribute("class", "episode-title");
-		episodeTitle.innerHTML = element.name
+		let season = element.season;
+		let eppy = element.number;
+		episodeTitle.innerHTML = `${element.name} - S${formatSeasonNum(season)}E${formatSeasonNum(eppy)}`
 		let episodeImg = document.createElement("img");
 		episodeImg.src = element.image.medium;
-		let episodeSummary = document.createElement("p");
+		let episodeSummary = document.createElement("div");
 		episodeSummary.innerHTML = element.summary;
-		episodeDiv.append(episodeTitle,episodeImg,episodeSummary);
+		episodeDiv.append(episodeTitle, episodeImg, episodeSummary);
 		episodesContainer.appendChild(episodeDiv)
 	})
-	
 	console.log (episodesContainer);
 }
 
